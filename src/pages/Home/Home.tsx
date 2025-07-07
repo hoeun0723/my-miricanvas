52e962d751ca4255e35a3a38c9878896f5b05764
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2025-07-07 16:10:28 +0900

    feat: sidebar UI 및 요소 클릭 기능 구현 (#4)
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import CanvasArea from '../../components/CanvasArea';
-import Sidebar from '../../components/Sidebar';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import Toolbar from '../../components/Toolbar';
 import styles from './Home.module.css';
 
