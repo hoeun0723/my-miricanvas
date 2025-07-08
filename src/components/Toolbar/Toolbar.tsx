8e37c2bdc90be299e9d22288db69b74a751d072e
Wang HoEun <hoeunwang@gmail.com>
2025-07-08 15:58:10 +0900

    feat: outro page 이동 버튼 생성

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import useThemeMode from '../../context/ThemeContext/useThemeMode';
 import { downloadCanvasAsPDF } from '../../utils/downloadPdf';
 import styles from './Toolbar.module.css';
 
 const Toolbar = () => {
+  const navigate = useNavigate();
   const { states, actions } = useThemeMode();
   const { isDarkMode } = states;
   const { themeToggler } = actions;
@@ -19,7 +21,9 @@ const Toolbar = () => {
       <span className={styles.title}>🧡 미니 미리캔버스</span>
 
       <div className={styles.actions}>
-        
+        <button onClick={()=>navigate('/outro')} className={styles.navigateButton}>
+          🖇️ outro 바로가기
+        </button>
         <button onClick={themeToggler} className={styles.themeToggleButton}>
           {isDarkMode ? '🌞 라이트 모드' : '🌙 다크 모드'}
         </button>
